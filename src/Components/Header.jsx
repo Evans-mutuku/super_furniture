0ba528f202110a214b86c063c8abb1af72945c10
evans <evans@pop-os.localdomain>
2022-01-28 21:55:50 +0300

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+  return <div>
+      <h3 className='text-green-500'>Hello world! Lets goooooooooo</h3>
+      <span></span>
+  </div>;
+};
+
+export default Header;
